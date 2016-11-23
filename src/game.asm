644e67e880989a57f94e74bb1104e26a1cbbbe83
LsW <rayniel@nauta.cu>
2016-11-23 15:53:41 -0500

    Se agregaron y probaron las funciones inRange y isValidPosition

--- src/game.asm
+++ src/game.asm
@@ -44,9 +44,33 @@ game:
       call get_input
 
     ; Main loop.
+     
+    push dword 3
+    push dword 2
+    push dword 255
+    push dword tablero
+    call putValue
+     
+    push dword tablero
+    call drawTablero
+     
+    push dword 80
+    push dword 24
+    push dword 0
+    push dword 0
+    push dword tablero
+    call isValidPosition
+    
+    cmp eax, 0
+    jne cosa
+    push dword 15
+    push dword 0
+    push dword 3
+    push dword 4
+    push dword 'd'
+    call putChar
+    cosa:
    
- 
-  
     ; Here is where you will place your game logic.
     ; Develop procedures like paint_map and update_content,
     ; declare it extern and use here.
@@ -75,22 +99,17 @@ get_input:
     ; The value of the input is on 'word [esp]'
 
     ; Your bindings here
-    cmp dword [page], 1
-    jne page2
-    mov eax, 0
-    bind KEY.DOWN, showMenu
-    bind KEY.DOWN.UP, showMenu
-    bind KEY.UP.UP, showMenu
-    bind KEY.UP, showMenu
-    bind KEY.ENTER, showMenu
-    cmp eax, 0
-    je continuePage1
-    push dword 15
-    push dword 0
-    push dword eax
-    push dword 0
-    push dword 'd'
-    call putChar
+;    cmp dword [page], 1
+;    jne page2
+;    mov eax, 0
+;    bind KEY.DOWN, showMenu
+;    bind KEY.DOWN.UP, showMenu
+;    bind KEY.UP.UP, showMenu
+;    bind KEY.UP, showMenu
+;    bind KEY.ENTER, showMenu
+;    cmp eax, 0
+;    je continuePage1
+    ; aca se actualiza la pagina y se hacen otros llamados en dependencia del valor de retorno del menu
     continuePage1:
     page2:
     add esp, 2 ; free the stack
@@ -176,8 +195,77 @@ putVerticalLine:
     pop ecx
     pop ebx
 ret 20
-
-
+; bool inRange(dword fila, dword columna, dword maxFila, dword maxColumna)
+; devuelve en eax 1, si fila y columna estan en el rango de la matriz con len filas = maxFila y 
+; y len columnas = maxColumna, 0 en caso contrario
+global inRange
+inRange:
+    push ebx
+    pushfd
+    push ebp
+    mov ebp, esp
+    
+    mov ebx, [ebp+16]
+    cmp ebx, 0
+    jl false2
+    cmp ebx, [ebp+24]
+    jge false2
+    mov ebx, [ebp+20]
+    cmp ebx, 0
+    jl false2
+    cmp ebx, [ebp+28]
+    jge false2
+    
+    true2:
+        mov eax, 1
+        jmp final3
+    false2:
+        xor eax, eax
+    final3:
+    pop ebp
+    popfd
+    pop ebx
+    
+ret 16
+; bool isValidPosition(dword tablero, dword fila, dword columna, dword maxFila, dword maxColumna)
+; verifica si en una matriz apuntada por tablero, en la posicion fila, columna existe o no un elemento
+; distinto de 0, devuelve 0 si hay un elemento distinto de 0, 1 en caso de que sea 0 el valor
+global isValidPosition
+isValidPosition:
+    push esi
+    push edx
+    pushfd
+    push ebp
+    mov ebp, esp
+    
+    push dword [ebp+36]
+    push dword [ebp+32]
+    push dword [ebp+28]
+    push dword [ebp+24]
+    call inRange
+    
+    cmp eax, 0
+    je false3
+    mov esi, [ebp+20]
+    mov eax, [ebp+36]
+    mov edx, [ebp+24]
+    mul edx
+    mov edx, [ebp+28]
+    add eax, edx
+    add esi, eax
+    cmp byte [esi], 0
+    jne false3
+    
+    mov eax, 1
+    jmp final4
+    false3:
+        xor eax, eax
+    final4:
+        pop ebp
+        popfd
+        pop edx
+        pop esi
+ret 20
     
     
     
